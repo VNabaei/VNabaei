6a9ee5a1a1d3b13c25acb85136720490b6e05d9d
VNabaei <v.nabaee@gmail.com>
2025-05-23 20:42:45 +0330

    Add files via upload

---
+++ WordAnalysis/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const NumberOfWord = document.querySelector('NumberOfWord');
+    let NumWord = document.getElementById("NumWord");
+    console.log(NumWord);
+
+});
